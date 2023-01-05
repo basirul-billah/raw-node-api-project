bec50c613a1d1af4ee64da2b3358e314f25e6fbb
Basirul Billah <basirulbillah@gmail.com>
2023-01-05 22:39:26 +0600

    added requestedProperties and callback

--- handlers/routeHandlers/sampleHandler.js
+++ handlers/routeHandlers/sampleHandler.js
@@ -8,8 +8,12 @@
 // Module scaffolding
 const handler = {};
 
-handler.sampleHandler = () => {
-    console.log('Sample');
-}
+handler.sampleHandler = (requestProperties, callback) => {
+    console.log(requestProperties);
+    
+    callback(200, {
+        message: 'This is a sample url',
+    });
+};
 
 module.exports = handler;
